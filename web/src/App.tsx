3f205ef50a598916f9ffd3114fce1d4dd49683b7
lebrancconvas <lebrancconvas@gmail.com>
2022-06-11 00:08:38 +0700

    ✨ Generated Image Feature.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios, { AxiosResponse } from 'axios';
+import { IData } from './lib/IData.interface';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [RNG, setRNG] = useState(0); // RNG: Random Number Generated.  
+
+  const generated = () => {
+    setRNG(Math.floor(Math.random() * data.length));
+  }
+  
+  useEffect(() => {
+    axios.get('http://localhost:9000/anya_pic')
+    .then((response: AxiosResponse) => response.data)
+    .then((data: any) => setData(data))
+    .catch((err: any) => console.error(err))
+  }, []);
+  
   return (
     <div className="App">
       <div>
-        <div></div>
+      {data.filter((_, index: number) => index === RNG).map((item: IData, index: number) => {
+        return(
+          <img src={item.url} alt="anya"/>
+        )
+      })}
       </div>
       <div>
-        <button>Generate</button>
+      <button onClick={generated}>Generate</button>
         <button>Download</button>
       </div>
     </div>
---
+++ web/src/lib/IData.interface.ts
@@ -0,0 +1,4 @@
+export interface IData {
+	id: number;
+	url: string;
+}
