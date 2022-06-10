42ca6c3b582e53b533ae46334a37842ed6453d35
lebrancconvas <lebrancconvas@gmail.com>
2022-06-10 23:12:25 +0700

    💄 Add Generate and Download Button to Website.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <div></div>
+      </div>
+      <div>
+        <button>Generate</button>
+        <button>Download</button>
+      </div>
     </div>
   );
 }
